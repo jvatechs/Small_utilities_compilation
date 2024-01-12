a2f7ddfe08c5b34238a39fb9e35c71ffe03e93e6
jvatechs <theusefulbrain@gmail.com>
2024-01-12 08:51:26 +0400

    Refactoring DocxParser and added several dependencies into pom.xml

--- src/main/java/org/jvatechs/wordDocx_parser_into_console/DocxParser.java
+++ src/main/java/org/jvatechs/wordDocx_parser_into_console/DocxParser.java
@@ -7,37 +7,55 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class DocxParser {
-    public static void main(String[] args) {
-        String initialDirectory = "C:/Users/Owner/Downloads";
+    private static final String INITIAL_DIRECTORY = "C:/Users/Owner/Downloads";
+    private static final String FILTER_DESCRIPTION = "Word Documents (*.docx)";
+    private static final String FILTER_EXTENSIONS = "docx";
 
-        JFileChooser fileChooser = new JFileChooser(initialDirectory);
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Word Documents (*.docx)", "docx");
-        // Show open dialog; this method does not return until the dialog is closed
-        fileChooser.setFileFilter(filter);
+    public static void main(String[] args) {
+        String text = new DocxParser().chooseFileAndReturnText();
+        System.out.println(text);
+    }
+
+    private String chooseFileAndReturnText() {
+        JFileChooser fileChooser = new JFileChooser(INITIAL_DIRECTORY);
+        applyFilters(fileChooser);
+
         int result = fileChooser.showOpenDialog(null);
 
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-            try {
-                // Create a FileInputStream to read the DOCX file
-                FileInputStream fis = new FileInputStream(selectedFile);
 
-                // Create XWPFDocument from the FileInputStream
-                XWPFDocument document = new XWPFDocument(fis);
-
-                // Create XWPFWordExtractor
-                XWPFWordExtractor extractor = new XWPFWordExtractor(document);
-                // Extract and print the text
-                String text = extractor.getText();
-                System.out.println(text);
-
-                // Close the InputStream
-                fis.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            return getTextFromDocxInputStream(selectedFile);
+        } else {
+            return null;
         }
     }
+
+    private String getTextFromDocxInputStream(File selectedFile) {
+        String text = null;
+        try {
+            FileInputStream fis = new FileInputStream(selectedFile);
+
+            XWPFDocument document = new XWPFDocument(fis);
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+
+            text = extractor.getText();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File doesn't exist. Please check and try it again.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+    private void applyFilters(JFileChooser fileChooser) {
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(FILTER_DESCRIPTION, FILTER_EXTENSIONS);
+        fileChooser.setFileFilter(filter);
+    }
+
+
 }
