bdb849a90888021a9e7c6464e0a5c1952038eec5
jvatechs <theusefulbrain@gmail.com>
2024-01-30 06:14:03 +0400

    deleted unnecessary class for more good refactoring

--- src/main/java/org/jvatechs/manipulations_with_csv/LocalMain.java
+++ src/main/java/org/jvatechs/manipulations_with_csv/LocalMain.java
@@ -1,7 +1,14 @@
 package org.jvatechs.manipulations_with_csv;
 
+import org.jvatechs.word_docx_to_txt_parser.DocxToTxtParser;
+
 public class LocalMain {
     public static void main(String[] args) {
+//        TextIntoCSVFormatter textIntoCSVFormatter = new TextIntoCSVFormatter();
+//        String text = new DocxToTxtParser().readFromTxt("word-to-txt.txt");
+//        textIntoCSVFormatter.createCSVFormattedText(text);
+//        textIntoCSVFormatter.saveResultIntoFile();
+
 //        GermanArticlesToCSVAdder germanArticlesToCsvAdder = new GermanArticlesToCSVAdder();
 //        germanArticlesToCsvAdder.init("translated.csv");
 
